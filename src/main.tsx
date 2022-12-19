8d20dcd1fd9c39d74045ce3d8ef7d491e31d23e4
Evgeny B <frenzerog@gmail.com>
2022-12-19 23:13:30 +0400

    Add redux-persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 import {Provider} from "react-redux";
 import store from "./store/store";
 import {BrowserRouter} from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
+import {persistStore} from "redux-persist";
+import {PersistGate} from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+let persistor = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor}/>
       <BrowserRouter>
         <ScrollToTop/>
         <App/>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
-)
+);
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,27 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import cartSlice from "./cart/cart-slice";
 import categoriesSlice from "./categories/categories-slice";
 import uiSlice from "./ui/ui-slice";
+import storage from "redux-persist/lib/storage";
+import {persistReducer} from "redux-persist";
+
+const reducers = combineReducers({
+  cart: cartSlice.reducer,
+  categories: categoriesSlice.reducer,
+  ui: uiSlice.reducer
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-    cart: cartSlice.reducer,
-    categories: categoriesSlice.reducer,
-    ui: uiSlice.reducer
-  }
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production"
 });
 
 export default store;
